07d59c73b9698962fb87b163802aa22924c7d2ed
Eka Setya Nugraha <ekasetyanugraha@gmail.com>
2020-12-13 15:35:13 +0700

    Add vue components: ContactListItem, MessageBubble

---
+++ src/vue/components/ContactListItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="p-4 is-clickable" style="border-bottom: 1px solid hsl(0, 0%, 86%);">
+    <div class="media">
+      <div class="media-left">
+        <span class="icon">
+          <i :class="iconClass" />
+        </span>
+      </div>
+      <div class="media-content">
+        <div>
+          {{ name }}
+          <span
+            v-if="lastMessageTime"
+            class="is-pulled-right has-text-grey-light is-size-7"
+          >
+            {{ lastMessageTime | toShortTime }}
+          </span>
+        </div>
+        <div class="has-text-grey-light">
+          <span v-if="lastMessageFromSelf">You: </span>{{ lastMessage }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { toShortTime } from '../../helpers/date';
+
+export default {
+  filters:{
+    toShortTime,
+  },
+  props: {
+    iconClass: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    lastMessage: {
+      type: String,
+      default: '',
+    },
+    lastMessageTime: {
+      type: Number,
+      default: null,
+    },
+    lastMessageFromSelf: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
---
+++ src/vue/components/MessageBubble.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    :class="{
+      'level-right': isFromSelf,
+    }"
+  >
+    <div
+      :class="{
+        'has-background-primary': isFromSelf,
+      }"
+      class="box is-inline-block p-2 mb-2"
+      style="min-width: 100px; max-width: 300px;"
+    >
+      <div
+        v-if="!isFromSelf"
+        class="has-text-primary has-text-weight-bold is-capitalized"
+      >
+        {{ title }}
+      </div>
+      <div
+        :class="{
+          'has-text-white': isFromSelf,
+        }"
+      >
+        {{ message }}
+      </div>
+      <div
+        :class="{
+          'has-text-grey-light': !isFromSelf,
+          'has-text-white-ter': isFromSelf,
+        }"
+        class="is-size-7"
+      >
+        {{ time | toShortTime }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { toShortTime } from '../../helpers/date';
+
+export default {
+  filters:{
+    toShortTime,
+  },
+  props: {
+    isFromSelf: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
