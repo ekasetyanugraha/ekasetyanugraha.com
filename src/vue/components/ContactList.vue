e05e42980daa1868e4cf1a336baf91dd57c2d11e
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-14 14:28:17 +0700

    Add component ContactList

---
+++ src/vue/components/ContactList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ContactListItem
+      v-for="contact in contacts"
+      :key="contact.name"
+      :contact="contact"
+      :messages="messages"
+      @click.native="onClickContact(contact)"
+    />
+  </div>
+</template>
+
+<script>
+import { getMyContacts, CONTACT_CODE_VUE } from '../../helpers/contacts';
+import ContactListItem from './ContactListItem';
+
+export default {
+  components: {
+    ContactListItem,
+  },
+  props: {
+    messages: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: () => ({
+    contacts: getMyContacts(CONTACT_CODE_VUE),
+  }),
+  methods: {
+    onClickContact(contact) {
+      this.$emit('click', contact);
+    },
+  },
+}
+</script>
