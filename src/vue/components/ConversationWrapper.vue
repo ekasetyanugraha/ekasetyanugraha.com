9c2a8d8fdf027bb0354b9b20f8a05e6361a78439
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-14 15:36:28 +0700

    Add empty state ConversationWrapper

--- src/vue/components/ConversationWrapper.vue
+++ src/vue/components/ConversationWrapper.vue
@@ -1,19 +1,30 @@
 <template>
   <div
     ref="messageContainer"
+    :class="{
+      'is-flex': !conversations.length,
+      'is-align-items-center': !conversations.length,
+    }"
     class="p-4"
     style="height: 500px; overflow-y: auto;"
   >
-    <transition-group name="slide-fade">
-      <MessageBubble
-        v-for="conversation in conversations"
-        :key="conversation.time"
-        :is-from-self="conversation.from === CONTACT_CODE_VUE"
-        :title="conversation.from"
-        :message="conversation.message"
-        :time="conversation.time"
-      />
-    </transition-group>
+    <div v-if="!conversations.length" class="has-text-centered">
+      <img src="/images/empty-chat-green.png" />
+      <p class="has-text-primary">Start chatting with <span class="is-capitalized">{{ to }}</span></p>
+    </div>
+
+    <template v-else>
+      <transition-group name="slide-fade">
+        <MessageBubble
+          v-for="conversation in conversations"
+          :key="conversation.time"
+          :is-from-self="conversation.from === CONTACT_CODE_VUE"
+          :title="conversation.from"
+          :message="conversation.message"
+          :time="conversation.time"
+        />
+      </transition-group>
+    </template>
   </div>
 </template>
 
