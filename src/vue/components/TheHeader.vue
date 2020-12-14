7a7bbbc56705b336a63b7c6cfe5d7a4dfe0d03ad
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-14 14:30:49 +0700

    Fix TheHeader style

--- src/vue/components/TheHeader.vue
+++ src/vue/components/TheHeader.vue
@@ -1,8 +1,37 @@
-<template functional>
+<template>
   <header class="card-image has-background-primary has-text-white p-5 is-size-4">
-    <span class="icon">
-      <i class="fab fa-vuejs"></i>
+    <figure class="image is-32x32 is-pulled-right">
+      <img :src="activeContact.img || CONTACT_IMAGE_SRC_VUE" class="is-rounded has-background-white">
+    </figure>
+    <span
+      v-if="activeContact.code"
+      class="icon is-clickable"
+      @click="onClickBack"
+    >
+      <i class="fas fa-chevron-left" />
     </span>
-    <span>Vue</span>
+    <span>{{ activeContact.name || CONTACT_NAME_VUE }}</span>
   </header>
 </template>
+
+<script>
+import { CONTACT_NAME_VUE, CONTACT_IMAGE_SRC_VUE } from '../../helpers/contacts';
+
+export default {
+  props: {
+    activeContact: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    CONTACT_NAME_VUE: () => CONTACT_NAME_VUE,
+    CONTACT_IMAGE_SRC_VUE: () => CONTACT_IMAGE_SRC_VUE,
+  },
+  methods: {
+    onClickBack() {
+      this.$emit('back');
+    },
+  },
+}
+</script>
