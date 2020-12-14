ca24f3123c9286da9b06cb336ce0e222404b9ace
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-14 17:10:13 +0700

    Fix React's TheHeader

--- src/react/components/TheHeader.jsx
+++ src/react/components/TheHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CONTACT_NAME_REACT, CONTACT_IMAGE_SRC_REACT } from '../../helpers/contacts';
 
 export default class TheHeader extends React.Component {
   constructor(props) {
@@ -8,10 +9,19 @@ export default class TheHeader extends React.Component {
   render() {
     return (
       <header className="card-image has-background-info has-text-white p-5 is-size-4">
-        <span className="icon">
-          <i className="fab fa-react"></i>
-        </span>
-        <span className="ml-2">React</span>
+        <figure className="image is-32x32 is-pulled-right">
+          <img src={this.props.activeContact.img || CONTACT_IMAGE_SRC_REACT} className="is-rounded has-background-white" />
+        </figure>
+        {
+          this.props.activeContact.code &&
+          <span
+            className="icon is-clickable"
+            onClick={this.props.onClickBack}
+          >
+            <i className="fas fa-chevron-left" />
+          </span>
+        }
+        <span className="ml-2">{ this.props.activeContact.name || CONTACT_NAME_REACT }</span>
       </header>
     );
   }
