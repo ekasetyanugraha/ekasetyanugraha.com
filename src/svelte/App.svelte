74f86ca08b25b87c850fb5a3682d3d957e2f9ec5
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-15 13:43:17 +0700

    Implement ConversationWrapper on App.svelte

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -3,6 +3,7 @@ import storeMessages from '../store/messages';
 import TheHeader from './components/TheHeader.svelte';
 import ContactList from './components/ContactList.svelte';
 import FormMessage from './components/FormMessage.svelte';
+import ConversationWrapper from './components/ConversationWrapper.svelte';
 
 let activeContact = {};
 let messages = [];
@@ -33,10 +34,9 @@ storeMessages.subscribe(onChangeMessages);
     style="min-height: 500px; overflow: hidden;"
   >
     {#if !activeContact.code}
-      <ContactList
-        {messages}
-        on:click={onClickContact}
-      />
+      <ContactList {messages} on:click={onClickContact} />
+    {:else}
+      <ConversationWrapper to={activeContact.code} {messages} />
     {/if}
   </section>
 
