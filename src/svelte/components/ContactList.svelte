55f1946b472743a40140996380d6d079e6ff3948
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-15 13:14:25 +0700

    Add svelte component: ContactList and ContactListItem

---
+++ src/svelte/components/ContactList.svelte
@@ -0,0 +1,19 @@
+<script>
+import { createEventDispatcher } from 'svelte';
+import { getMyContacts, CONTACT_CODE_SVELTE } from '../../helpers/contacts';
+import ContactListItem from './ContactListItem';
+
+export let messages = [];
+
+const dispatch = createEventDispatcher();
+
+const contacts = getMyContacts(CONTACT_CODE_SVELTE);
+</script>
+
+{#each contacts as contact}
+  <ContactListItem
+    {contact}
+    {messages}
+    on:click={() => dispatch('click', contact)}
+  />
+{/each}
---
+++ src/svelte/components/ContactListItem.svelte
@@ -0,0 +1,46 @@
+<script>
+import { createEventDispatcher } from 'svelte';
+import { CONTACT_CODE_SVELTE } from '../../helpers/contacts';
+import { toShortTime } from '../../helpers/date';
+import { getContactConversationsLastMessage } from '../../helpers/messages';
+
+export let contact = {};
+export let messages = [];
+
+const dispatch = createEventDispatcher();
+
+$: lastConversation = getContactConversationsLastMessage(messages, CONTACT_CODE_SVELTE, contact.code);
+$: lastMessage = lastConversation.message || '';
+$: lastMessageTime = lastConversation.time;
+$: lastMessageFromSelf = lastConversation.from === CONTACT_CODE_SVELTE;
+</script>
+
+<div
+  class="p-4 is-clickable"
+  style="border-bottom: 1px solid hsl(0, 0%, 86%);"
+  on:click={() => dispatch('click')}
+>
+  <div class="media">
+    <div class="media-left">
+      <figure class="image is-32x32">
+        <img src={contact.img} alt={contact.name} class="is-rounded">
+      </figure>
+    </div>
+    <div class="media-content">
+      <div>
+        { contact.name }
+        {#if lastMessageTime}
+          <span class="is-pulled-right has-text-grey-light is-size-7">
+            { toShortTime(lastMessageTime) }
+          </span>
+        {/if}
+      </div>
+      <div class="has-text-grey-light is-text-ellipsis">
+        {#if lastMessageFromSelf}
+          <span>You: </span>
+        {/if}
+        { lastMessage }
+      </div>
+    </div>
+  </div>
+</div>
