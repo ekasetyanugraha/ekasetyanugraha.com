bf2ee0d02b283e92d6b7c118dc39bc00d0acad77
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-15 13:16:24 +0700

    Implement Svelte ContactList on App.svelte

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -1,15 +1,48 @@
 <script>
+import storeMessages from '../store/messages';
 import TheHeader from './components/TheHeader.svelte';
+import ContactList from './components/ContactList.svelte';
 import FormMessage from './components/FormMessage.svelte';
+
+let activeContact = {};
+let messages = [];
+
+const onChangeMessages = () => {
+  messages = storeMessages.getState();
+};
+
+const onClickBack = () => {
+  activeContact = {};
+};
+
+const onClickContact = event => {
+  activeContact = event.detail;
+};
+
+storeMessages.subscribe(onChangeMessages);
 </script>
 
 <div class="card">
-  <TheHeader />
+  <TheHeader
+    {activeContact}
+    on:back={onClickBack}
+  />
 
-  <section class="card-content p-0" style="min-height: 500px;">
+  <section
+    class="card-content p-0"
+    style="min-height: 500px; overflow: hidden;"
+  >
+    {#if !activeContact.code}
+      <ContactList
+        {messages}
+        on:click={onClickContact}
+      />
+    {/if}
   </section>
 
-  <footer class="card-footer p-3">
-    <FormMessage />
-  </footer>
+  {#if activeContact.code}
+    <footer class="card-footer p-3">
+      <FormMessage to={activeContact.code} />
+    </footer>
+  {/if}
 </div>
