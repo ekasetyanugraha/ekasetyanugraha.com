ab98ccb12108498af7a8de989167bb5b3a63504a
Eka Setya Nugraha <eka.setya@bukalapak.com>
2020-12-15 13:43:06 +0700

    Add svelte component: ConversationWrapper and MessageBubble

---
+++ src/svelte/components/ConversationWrapper.svelte
@@ -0,0 +1,42 @@
+<script>
+import { afterUpdate } from 'svelte';
+import { CONTACT_CODE_SVELTE } from '../../helpers/contacts';
+import { getContactConversations } from '../../helpers/messages';
+import { scrollElementToBottom } from '../../helpers/element';
+import MessageBubble from './MessageBubble';
+
+export let to = '';
+export let messages = [];
+
+let messageContainer;
+
+afterUpdate(() => {
+  scrollElementToBottom(messageContainer);
+});
+
+$: conversations = getContactConversations(messages, CONTACT_CODE_SVELTE, to);
+</script>
+
+<div
+  bind:this={messageContainer}
+  class:is-flex={!conversations.length}
+  class:is-align-items-center={!conversations.length}
+  class="p-4"
+  style="height: 500px; overflow-y: auto;"
+>
+  {#if !conversations.length}
+    <div class="has-text-centered">
+      <img src="/images/empty-chat-red.png" alt="Empty Chat" />
+      <p class="has-text-danger">Start chatting with <span class="is-capitalized">{ to }</span></p>
+    </div>
+  {:else}
+    {#each conversations as conversation}
+      <MessageBubble
+        isFromSelf={conversation.from === CONTACT_CODE_SVELTE}
+        title={conversation.from}
+        message={conversation.message}
+        time={conversation.time}
+      />
+    {/each}
+  {/if}
+</div>
---
+++ src/svelte/components/MessageBubble.svelte
@@ -0,0 +1,37 @@
+<script>
+import { toShortTime } from '../../helpers/date';
+
+export let isFromSelf = false;
+export let title = '';
+export let message = '';
+export let time = null;
+</script>
+
+<div
+  class:level-right={isFromSelf}
+>
+  <div
+    class:has-background-danger={isFromSelf}
+    class="box is-inline-block p-2 mb-2"
+    style="min-width: 100px; max-width: 300px;"
+  >
+    {#if !isFromSelf}
+      <div class="has-text-danger has-text-weight-bold is-capitalized">
+        { title }
+      </div>
+    {/if}
+    <div
+      class:has-text-white={isFromSelf}
+      style="word-break: break-word;"
+    >
+      { message }
+    </div>
+    <div
+      class:has-text-grey-light={!isFromSelf}
+      class:has-text-white-ter={isFromSelf}
+      class="is-size-7"
+    >
+      { toShortTime(time) }
+    </div>
+  </div>
+</div>
